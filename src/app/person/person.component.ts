d10887996fd588045a9f39ae8b71137977604cf4
lumyrani <lumyrani@gmail.com>
2021-09-25 10:09:36 -0500

    first commit

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -30,6 +30,7 @@ export class PersonComponent implements OnInit {
   })
 
   onSubmit() {
+    //if isEdit true , then submit button works
     if (!this.isEdit) {
       console.log("this.personObj._id ")
       this.personObj.name = this.personForm.value.name
@@ -38,26 +39,40 @@ export class PersonComponent implements OnInit {
       this.personObj.pin = this.personForm.value.pin
       this.personService.postPerson(this.personObj)
         .subscribe(
-          (res) => { alert('inserted'), this.loadpersons() },
+          (res) => {
+            alert('inserted'),
+              // loading new form data details
+              this.loadpersons()
+          },
           (err) => { alert('not inserted') }
 
         )
-    } else {
+    }
+    else {
 
+      //to get data for edit
       this.personObj.name = this.personForm.value.name
       this.personObj.place = this.personForm.value.place
       this.personObj.state = this.personForm.value.state
       this.personObj.pin = this.personForm.value.pin
 
-      this.personService.updatePerson(this.personObj, this.personObj._id)
-        .subscribe((res) => {
-          alert('updated')
-          this.loadpersons()
-        })
+
+      this.onEdit(this.personObj)
+      alert('updated')
+
+      //to make isEdit false then  buttton save will appear
+      this.isEdit = false
+
+      //to make form empty
+      this.personObj.name = ""
+      this.personObj.place = ""
+      this.personObj.state = ""
+      this.personObj.pin = ""
+
+      this.loadpersons()
+
 
     }
-    this.isEdit = false
-    this.personForm.reset()
 
   }
   loadpersons() {
@@ -69,18 +84,18 @@ export class PersonComponent implements OnInit {
     )
   }
 
+  //to delete data with given form data id 
   onDelete(pers: any) {
     this.personService.deletePerson(pers._id)
-      .subscribe(
-        (res) => {
-          console.log("deleted")
-          alert('deleted'), this.loadpersons()
-        })
+        .subscribe((res) => {
+        console.log("deleted")
+        alert('deleted'), this.loadpersons()
+      })
   }
 
+  // make form data details empty
   reset() {
     this.personForm.controls[""]
-    this.personForm.controls['name'].setValue("")
     this.personForm.controls['name'].setValue("")
     this.personForm.controls['place'].setValue("")
     this.personForm.controls['state'].setValue("")
@@ -89,18 +104,21 @@ export class PersonComponent implements OnInit {
   }
 
   onEdit(pers: any) {
+
     this.isEdit = true
     this.personService.updatePerson(pers, pers._id).
       subscribe((response) => {
         this.personObj._id = pers._id
         this.personForm.controls['name'].setValue(pers.name)
-        this.personForm.controls['name'].setValue(pers.name)
         this.personForm.controls['place'].setValue(pers.place)
         this.personForm.controls['state'].setValue(pers.state)
         this.personForm.controls['pin'].setValue(pers.pin)
+        this.loadpersons()
+
       })
 
   }
+
 }
 
 
